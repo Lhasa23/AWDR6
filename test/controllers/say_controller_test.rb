899268b72fac1dd1f4f7d3be46023f64094a4717
shiyi <sydryy@gmail.com>
2021-09-16 09:40:48 +0800

    develop complete 2-2

---
+++ test/controllers/say_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class SayControllerTest < ActionDispatch::IntegrationTest
+  test "should get hello" do
+    get say_hello_url
+    assert_response :success
+  end
+
+  test "should get goodbye" do
+    get say_goodbye_url
+    assert_response :success
+  end
+end
