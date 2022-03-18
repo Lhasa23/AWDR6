76b8812b8525c92854e5715204d7031a2a3f5265
LAk <1547764705@qq.com>
2022-03-18 09:30:07 +0800

    product viladate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_14_013003) do
+ActiveRecord::Schema.define(version: 2022_01_18_003423) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
