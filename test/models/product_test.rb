afb8722d137f52d4836227d4f1ed2bd9edebd47f
huzhuangzhuang <hu_zhuangzhuang@163.com>
2022-03-29 09:31:45 +0800

    image url unit test

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -19,28 +19,33 @@ class ProductTest < ActiveSupport::TestCase
     product.price = -1
     assert product.invalid?
     assert_equal ['must be greater than or equal to 0.01'],
-                 product.errors[:price]
+                  product.errors[:price]
     product.price = 0
     assert product.invalid?
     assert_equal ['must be greater than or equal to 0.01'],
-                 product.errors[:price]
+                  product.errors[:price]
     product.price = 1
     assert product.valid?
   end
 
   def new_product(image_url)
     Product.new(title: 'My Book Title',
-                description: '12312313',
+                description: 'yyy',
                 price: 1,
                 image_url: image_url)
   end
 
+<<<<<<< HEAD
   test 'image url' do
     ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
            http://a.b.c/x/y/z/fred.gif }
+=======
+  test "image url" do
+    ok = %w{ fred.gif fred.jpg fred.png FRED.GIF FRED.Jpg http://a.b.c/x/y/z/fred.gif }
+>>>>>>> e347bfa (image url unit test)
     bad = %w{ fred.doc fred.gif/more fred.gif.more }
     ok.each do |image_url|
-      assert new_product(image_url).valid?
+      assert new_product(image_url).valid?, "#{image_url} shouldn't be invalid"
     end
     bad.each do |image_url|
       assert new_product(image_url).invalid?, "#{image_url} shouldn't be valid"
