9f56f1ee52987634be01c52a199834291ecd63a6
shiyi <sydryy@gmail.com>
2022-04-19 09:46:58 +0800

    functional test

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,5 +4,9 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get store_index_url
     assert_response :success
+    assert_select 'nav.side_nav ul li', 4
+    assert_select 'ul.catalog li', minimum: 3
+    assert_select 'ul.catalog li h2', 'MyString1'
+    assert_select 'ul.catalog li .price', /^\$[,\d]+\.\d{2}$/
   end
 end
