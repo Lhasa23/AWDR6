ed0e6f2f660a461459d562665ca201bb85bb91d4
shiyi <sydryy@gmail.com>
2022-06-10 09:44:36 +0800

    generate line items init status

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,5 @@
 class Product < ApplicationRecord
   has_many :line_items
-  before_destroy :ensure_not_referenced_by_any_line_item
 
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
@@ -13,11 +12,4 @@ class Product < ApplicationRecord
     minimum: 10,
     too_short: "title length must be greater than or equal %{count}"
   }
-  def ensure_not_referenced_by_any_line_item
-    unless line_items.empty?
-      errors.add(:base, 'Line Items present')
-      throw :abort
-    end
-
-  end
 end
