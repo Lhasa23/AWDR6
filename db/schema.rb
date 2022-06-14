77da780f566cb3a552acecf65fc6e5876e1cf21b
LAk <1547764705@qq.com>
2022-06-14 09:45:32 +0800

    line item model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_18_003423) do
+ActiveRecord::Schema.define(version: 2022_06_10_005110) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
