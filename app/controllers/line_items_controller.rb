6471cab7b71d76404c8abcfbaaea44b625ffb59b
LAk <1547764705@qq.com>
2022-06-23 09:28:05 +0800

    cartproduct

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -3,7 +3,7 @@ class LineItemsController < ApplicationController
 
   # GET /line_items or /line_items.json
   def index
-    @line_items = LineItem.all
+    @line_items = @cart.line_items
   end
 
   # GET /line_items/1 or /line_items/1.json
