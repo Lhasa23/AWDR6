247ddd4d6cc3ce77301797bf7419c3d1eb2a3672
LAk <1547764705@qq.com>
2022-08-04 09:38:44 +0800

    merge product_id and quantity

--- db/migrate/20220802005114_line_item_quantity_merge.rb
+++ db/migrate/20220802005114_line_item_quantity_merge.rb
@@ -2,8 +2,10 @@ class LineItemQuantityMerge < ActiveRecord::Migration[6.1]
   def up
     Cart.all.each do |cart|
       products_quantity = cart.line_items.group(:product_id).sum(:quantity)
-      products_quantity.each do |product_id, products_quantity|
+      products_quantity.each do |product_id, quantity|
         if quantity > 1
+          cart.line_items.where(product_id: product_id).delete_all
+          LineItem.create(product_id: product_id, quantity: quantity, cart_id: cart.id)
         end
       end
     end
