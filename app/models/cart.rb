c95087badb80b645180c8de0b0b5fbcac0f00d3b
LAk <1547764705@qq.com>
2022-08-09 09:34:54 +0800

    cart reconstruction

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,14 @@
 class Cart < ApplicationRecord
   has_many :line_items, dependent: :destroy
+
+  def add_product(product)
+    line_item = LineItem.find_by(cart_id: id, product_id: product.id)
+    if line_item.nil?
+      line_item = line_items.build(product: product)
+    else
+      line_item.quantity += 1
+    end
+    line_item
+  end
 end
+
