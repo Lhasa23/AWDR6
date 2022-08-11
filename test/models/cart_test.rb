fa109db901b19d3c68abfe09491c2710c33eddbd
LAk <1547764705@qq.com>
2022-08-11 09:44:12 +0800

    add test remove modles

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class CartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "add_product twice" do
+    product = products(:one)
+    cart = carts(:one)
+    2.times { cart.add_product(product).save }
+
+    line_items = cart.line_items.where(product_id: product.id)
+    assert_equal(1, line_items.size)
+    assert_equal(2, line_items.first.quantity)
+  end
+
+  test "add_product when line_item not exists" do
+    product = products(:one)
+    cart = carts(:one)
+    cart.add_product(product).save
+
+    line_items = cart.line_items.where(product_id: product.id)
+    assert_equal(1, line_items.size)
+    assert_equal(1, line_items.first.quantity)
+  end
 end
