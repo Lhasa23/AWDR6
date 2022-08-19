700f01ade8b8f8fa7ad8bb6b409b768637b9bdf4
eleven <sydryy@gmail.com>
2022-08-18 18:25:41 -0700

    rename scoped cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,12 +1,12 @@
 class CartsController < ApplicationController
   before_action :set_current_cart, only: [:show, :destroy]
   def show
-    @line_items = @cart.line_items
+    @line_items = @current_cart.line_items
   end
 
   def destroy
-    @cart.line_items.delete_all
-    @line_items = @cart.line_items
+    @current_cart.line_items.delete_all
+    @line_items = @current_cart.line_items
 
     respond_to do |format|
       format.js
@@ -16,6 +16,6 @@ class CartsController < ApplicationController
   private
 
   def set_current_cart
-    @cart = Cart.find(params[:id])
+    @current_cart = Cart.find(params[:id])
   end
 end
