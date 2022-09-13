f8cc0ac900f90e6a04a516ce46188182c632af86
LAk <1547764705@qq.com>
2022-09-13 09:26:22 +0800

    删除迁移数据中注释代码

--- db/migrate/20220908011325_line_item_price_migrate.rb
+++ db/migrate/20220908011325_line_item_price_migrate.rb
@@ -1,13 +1,8 @@
 class LineItemPriceMigrate < ActiveRecord::Migration[6.1]
   def up
-    # LineItem.all.each do |line_item|
-    #   line_item.price = line_item.product.price
-    #   line_item.save!
-    # end
     Product.all.each do |product|
       product.line_items.update_all price: product.price
     end
-
   end
 
   def down
