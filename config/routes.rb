a1fa0a55574a76ae299c2797cc7cf8e44823b803
huzhuangzhuang <zhuangzhuang@dao42.com>
2022-11-15 09:03:56 +0800

    member route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   root 'store#index', as: :store_index
   resources :products
-  resources :line_items, only: [:create, :update, :destroy]
-  get '/line_items/:id/refresh', to: "line_items#refresh"
   resources :carts, only: [:show, :destroy]
+  resources :line_items do
+    member do
+      get 'refresh'
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
